abe958cca8d8907c836f23eab3292b926e85927a
NanthawatKTY <nanthawat.tae123@gmail.com>
2019-10-29 19:02:48 +0700

    Initial commit

---
+++ Assets/Scripts/NumberVote.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class NumberVote : MonoBehaviour
+{
+
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI voteText;
+
+    int guess;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+
+        NextGuess();
+    }
+
+    public void OnPressVoteUp()
+    {
+        min = guess + 1;
+        NextGuess();
+        
+    }
+
+    public void OnPressVoteDown()
+    {
+        max = guess ;
+        NextGuess();
+    }
+    void NextGuess()
+    {
+        if (min < max)
+        {
+            guess = Random.Range(min, max);
+        }
+        else if (min == max)
+        {
+            guess = min;
+        }
+        else
+        {
+            min = guess;
+        }
+
+            voteText.text = guess.ToString();
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        //int cuz SceneLoader is 1,2,3 or 4 integer
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;  //Scene we are on now
+        SceneManager.LoadScene(currentSceneIndex + 1); //Cuz currentSceneIndex = 0 then +1+2+3+...+ X
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0); //Load Start Scene [0] (index = 0)
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
